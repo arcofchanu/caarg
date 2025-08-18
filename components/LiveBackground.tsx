4e58ca9bc901c0446276476d72c2b2af0eb7d677
Ronit Suthar <67582658+arcofchanu@users.noreply.github.com>
2025-08-18 23:48:52 +0530

    Update LiveBackground.tsx

--- components/LiveBackground.tsx
+++ components/LiveBackground.tsx
@@ -136,7 +136,7 @@ const LiveBackground: React.FC = () => {
     };
   }, []);
 
-  return <canvas ref={canvasRef} className="fixed top-0 left-0 w-full h-full z-0 bg-black" />;
+  return <canvas ref={canvasRef} className="absolute top-0 left-0 w-full h-full z-0 bg-black" />;
 };
 
 export default LiveBackground;
