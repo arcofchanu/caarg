e34226888e16daf579d007f7b130a87610067eb5
Ronit Suthar <67582658+arcofchanu@users.noreply.github.com>
2025-08-19 00:50:02 +0530

    Update IconComponents.tsx

--- components/IconComponents.tsx
+++ components/IconComponents.tsx
@@ -12,12 +12,12 @@ export const UserIcon: React.FC<IconProps> = ({ className }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth="0.5"
     strokeLinecap="round"
     strokeLinejoin="round"
   >
     <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
-    <circle cx="12" cy="7" r="4" />
+    <circle cx="12" cy="7" r="7" />
   </svg>
 );
 
