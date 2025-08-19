624421bbf1e5adb6d04f1a68ef8b1c0ce5cef53c
Ronit B Suthar <67582658+arcofchanu@users.noreply.github.com>
2025-08-20 02:51:23 +0530

    Update IconComponents.tsx

--- components/IconComponents.tsx
+++ components/IconComponents.tsx
@@ -46,3 +46,18 @@ export const SendIcon: React.FC<IconProps> = ({ className }) => (
       <path d="M22 2 11 13" />
     </svg>
 );
+
+export const SpinnerIcon: React.FC<IconProps> = ({ className }) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
+);
